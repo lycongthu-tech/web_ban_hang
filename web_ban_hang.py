a4fb2f08171c7416d0f1403795958b22292462e8
lycongthuc <lycongthu@gmail.com>
2026-09-20 17:33:48 +0700

    fix

--- web_ban_hang.py
+++ web_ban_hang.py
@@ -245,7 +245,7 @@ def trang_chu():
                 </div>
                 <!-- BONG BÓNG ZALO LIÊN KẾT NỔI -->
                     <a href="https://zalo.me/0962731032" target="_blank" class="zalo-bubble" title="Chat qua Zalo">
-                        <img src="https://download.com.vn" alt="Zalo">
+                        <img src="https://download.com.vn" alt="Zalo" style="width: 35px !important; height: 35px !important; opacity: 1 !important; filter: none !important; object-fit: contain; display: block; margin: auto;">
                     </a>
 
                     <!-- FOOTER -->
